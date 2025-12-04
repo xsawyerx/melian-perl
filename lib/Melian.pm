7cd305e8bd067f734fda262434c00d11ec924148
Sawyer X <xsawyerx@cpan.org>
2025-12-04 22:47:45 +0100

    New functions, faster code:
    * We now have fetch_by_int_from() and fetch_by_string_from()
    * They receive table name and column name, figure out the ID themselves
    * There are now tests for it
    * We only call connect() once instead of continuously

--- lib/Melian.pm
+++ lib/Melian.pm
@@ -2,11 +2,12 @@ package Melian;
 # ABSTRACT: Perl client to the Melian cache
 
 use v5.34;
-use JSON::XS qw<decode_json>;
+use Carp qw(croak);
 use IO::Socket::INET;
 use IO::Socket::UNIX;
+use JSON::XS qw(decode_json);
+use List::Util qw(first);
 use Socket qw(SOCK_STREAM);
-use Carp qw(croak);
 use constant {
     'MELIAN_HEADER_VERSION' => 0x11,
     'ACTION_FETCH'          => ord('F'),
@@ -28,6 +29,8 @@ sub new {
     my @schema_args = grep /^schema(?:_spec|_file)?$/, keys %args;
     @schema_args > 1
         and croak(q{Provide maximum one of: 'schema', 'schema_spec', 'schema_file'});
+
+    $self->connect();
 
     if (my $schema = $args{'schema'}) {
         $self->{'schema'} = $schema;
@@ -80,12 +83,35 @@ sub disconnect {
     return;
 }
 
+sub fetch_raw_from {
+    my ( $self, $table_name, $column_name, $key ) = @_;
+
+    # Get table ID
+    my $table = $self->_get_table($table_name);
+    my $table_id = $table->{'id'};
+
+    # Get column ID
+    my $column_id = $self->_get_column_id( $table, $column_name );
+    return $self->fetch_raw( $table_id, $column_id, $key );
+}
+
 sub fetch_raw {
-    my ($self, $table_id, $index_id, $key) = @_;
-    $self->connect();
+    my ( $self, $table_id, $index_id, $key ) = @_;
     defined $key
         or croak("You must provide a key to fetch");
     return $self->_send(ACTION_FETCH(), $table_id, $index_id, $key);
+}
+
+sub fetch_by_string_from {
+    my ( $self, $table_name, $column_name, $key ) = @_;
+
+    # Get table ID
+    my $table = $self->_get_table($table_name);
+    my $table_id = $table->{'id'};
+
+    # Get column ID
+    my $column_id = $self->_get_column_id( $table, $column_name );
+    return $self->fetch_by_string( $table_id, $column_id, $key );
 }
 
 sub fetch_by_string {
@@ -105,15 +131,27 @@ sub fetch_by_string {
     return $decoded;
 }
 
+sub fetch_by_int_from {
+    my ( $self, $table_name, $column_name, $id ) = @_;
+
+    # Get table ID
+    my $table = $self->_get_table($table_name);
+    my $table_id = $table->{'id'};
+
+    # Get column ID
+    my $column_id = $self->_get_column_id( $table, $column_name );
+
+    return $self->fetch_by_int( $table_id, $column_id, $id );
+}
+
 sub fetch_by_int {
-    my ($self, $table_id, $index_id, $id) = @_;
-    return $self->fetch_by_string($table_id, $index_id, pack('V', $id));
+    my ($self, $table_id, $column_id, $id) = @_;
+    return $self->fetch_by_string($table_id, $column_id, pack('V', $id));
 }
 
 sub _load_schema_from_describe {
     my $self = shift;
 
-    $self->connect();
     my $payload = $self->_send(ACTION_DESCRIBE(), 0, 0, '');
     defined $payload && length $payload
         or croak('Could not get schema data');
@@ -184,7 +222,6 @@ sub _load_schema_from_spec {
 sub _send {
     my ( $self, $action, $table_id, $index_id, $payload ) = @_;
     $payload //= '';
-    $self->connect();
     defined $table_id && defined $index_id
         or croak("Invalid table ID or index ID");
 
@@ -227,6 +264,30 @@ sub _read_exactly {
     }
 
     return $buffer;
+}
+
+sub _get_table {
+    my ( $self, $name ) = @_;
+    my $table = List::Util::first(
+        sub { $_->{'name'} eq $name },
+        @{ $self->{'schema'}{'tables'} },
+    );
+
+    $table or croak("Cannot find table named '$name'");
+    return $table;
+}
+
+sub _get_column_id {
+    my ( $self, $table, $name ) = @_;
+
+    # Get column ID
+    my $column = List::Util::first(
+        sub { $_->{'column'} eq $name },
+        @{ $table->{'indexes'} },
+    );
+
+    $column or croak("Cannot find column named '$name'");
+    return $column->{'id'};
 }
 
 1;
@@ -285,8 +346,8 @@ Logic for handling schema:
 
     $client->connect();
 
-Explicitly opens the underlying socket if it is not already connected. Usually
-called automatically by fetch routines.
+Explicitly opens the underlying socket if it is not already connected. Called
+automatically when create a new Melian instance.
 
 =head2 disconnect
 
